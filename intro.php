1b46ad83cc52fb4b8f2b32d0dce267979b394a1c
primusr <primus.reyes@gmail.com>
2025-10-01 10:46:07 +0800

    discaya is the limit

--- intro.php
+++ intro.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h3>Task 1. Introduce Yourself</h3>
 <?php
 
     $name="Curly Discaya";
@@ -5,4 +15,8 @@
 
     echo "HI, I am " . $name;
 
-?>
+    ?>
+
+</body>
+</html>
+
---
+++ math.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task 2. Simple Math</title>
+</head>
+<body>
+<h3>Task 2. Simple Math</h3>
+
+<?php 
+    
+    $a=100;
+    $b=15;
+
+    $sum=$a+$b;
+    $difference=$a-$b;
+    $product=$a*$b;
+    $quot=$a/$b;
+
+    echo "Value of A: " . $a  . "<br>";
+    echo "Value of B: " . $b . "<br>"; 
+    echo "The sum is: " . $sum . "<br>"; 
+    echo "The difference is: " . $difference . "<br>";
+    echo "The product is: " . $product . "<br>";
+    echo "The difference is: " . $quot . "<br>";
+
+
+
+?>
+
+</body>
+</html>
+
+
